d1881b0530235a872e1796fd3dab8ccf6c96d3b8
Carlos Calderon <cdcalderon@gmail.com>
2017-10-01 12:51:12 -0500

    sync

--- dotnetcore-sample/Startup.cs
+++ dotnetcore-sample/Startup.cs
@@ -12,6 +12,7 @@ namespace dotnetcore_sample
 {
     public class Startup
     {
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -30,7 +31,7 @@ namespace dotnetcore_sample
             // Add framework services.
             services.AddMvc();
         }
-
+         
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
